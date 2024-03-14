8cb23b20b2170a03b6145a4928b1e4b0d57a7276
BK-222 <58715574+BK-222@users.noreply.github.com>
2024-03-14 19:14:32 +0000

    second

--- src/router.js
+++ src/router.js
@@ -1,12 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Main from './pages/main.vue'
+import About from './pages/about.vue'
+import Projects from './pages/projects.vue'
+import DevSkills from './pages/dev-skills.vue'
+import Contact from './pages/contact.vue'
 
 const router = createRouter({
   history: createWebHistory(), 
   routes: [
     { path: '/', redirect: 'main' }, 
-    { path: '/main', component: Main }
+    { path: '/main', component: Main },
+    { path: '/about', component: About }, 
+    { path: '/projects', component: Projects }, 
+    { path: '/dev-skills', component: DevSkills },
+    { path: '/contact', component: Contact }
   ]
 })
 

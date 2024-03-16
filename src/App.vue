1bc02e4877a1aca8b1fcc3c51ac70529ae195265
BK-222 <58715574+BK-222@users.noreply.github.com>
2024-03-16 17:52:06 +0000

    added footer and header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
+  
+import TheHeader from './layouts/TheHeader.vue'
+import TheFooter from './layouts/TheFooter.vue'
+
 </script>
 
 <template>
   <router-view></router-view>
+  <TheFooter />
 </template>
--- src/stores/projectsStore.js
+++ src/stores/projectsStore.js
@@ -1,5 +1,57 @@
 import { defineStore } from 'pinia'
 
+import { reactive, computed } from 'vue' 
+
 const useProjectsStore = defineStore('projectsStore', () => {
+  const projects = reactive([
+    {
+      id: 1,
+      title: 'Simple playable battle app.',
+      img_1: '_img/screens/slayer_1.png',
+      img_2: '_img/screens/slayer_2.png',
+      img_3: '_img/screens/slayer_3.png',
+      link: 'https://priceless-edison-9dd932.netlify.app/',
+      gitHub: 'https://github.com/BK-222/tms',
+      tech: 'Vue'
+    },
+    {
+      id: 2,
+      title: 'Discord bot',
+      img_1: '_img/screens/beato_1.png',
+      img_2: '_img/screens/beato_2.png',
+      img_3: '_img/screens/beato_3.png',
+      link: 'https://github.com/BK-222/discord-bot',
+      gitHub: 'https://github.com/BK-222/discord-bot',
+      tech: 'Node.js'
+    },
+    {
+      id: 3,
+      title: 'Landing page',
+      img_1: '_img/screens/landing_1.png',
+      img_2: '_img/screens/landing_2.png',
+      img_3: '_img/screens/landing_3.png',
+      link: 'https://bk-222.github.io/animeet/',
+      gitHub: 'https://github.com/BK-222/animeet',
+      tech: 'Plain HTML/CSS with .js scroller'
+    },
+    {
+      id: 4,
+      title: 'A Small Register/Book Practice Project',
+      img_1: '_img/screens/prac_1.png',
+      img_2: '_img/screens/prac_2.png',
+      img_3: '_img/screens/prac_3.png',
+      link: 'https://github.com/BK-222/akiba-place',
+      gitHub: 'https://github.com/BK-222/akiba-place',
+      tech: 'Mevn'
+    }
+  ])
+    
+  const stateProjects = computed(() => {
+    return projects.value;
+  })
+
+  return { projects, stateProjects }
   
 }) 
+
+export default useProjectsStore;

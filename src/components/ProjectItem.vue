3633fc1e68026edf028808db69c687a4b3e0d519
BK-222 <58715574+BK-222@users.noreply.github.com>
2024-03-17 22:05:29 +0000

    (temp. update)

---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,29 @@
+<script setup>
+  
+const props = defineProps({
+  title: { type: String, required: true },
+  img_1: { type: String, required: true },
+  img_2: { type: String, required: true },
+  img_3: { type: String, required: true },
+  link: { type: String, required: true },
+  gitHub: { type: String, required: true },
+  tech: { type: String, required: true }
+});
+
+</script>
+
+<template>
+  <div>
+    <base-card class="group transition delay-75 border border-transparent hover:border-cyan-500">
+      <h3 class="mb-2 text-lg font-bold group-hover-text-cyan-500 delay-100">{{ title }}</h3>
+      <div class="flex my-3.5">
+        <div class="bg-norepeat bg-cover bg-center h-16 w-24 mr-3 cursor-pointer transform hover:scale-105" :style="{ \'background-image\': `url(${img_1})` }"></div>
+        <div class="bg-norepeat bg-cover bg-center h-16 w-24 mr-3 cursor-pointer transform hover:scale-105 hover:cursor-pointer" :style="{ \'background-image\': `url(${img_2})` }"></div>
+        <div class="bg-norepeat bg-cover bg-center h-16 w-24 cursor-pointer transform hover:scale-105 hover:cursor-pointer" :style="{ \'background-image\': `url(${img_3})` }"></div>
+      </div>
+      <base-link external><a :href="link" target="_blank">link</a></base-link>
+      <base-link external><a :href="gitHub" target="_blank">code</a></base-link>
+      <p class="mt-1 font-semibold">{{ tech }}</p>
+    </base-card>
+  </div>
+</template>
--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,7 +1,37 @@
 <script setup>
 
+import useProjectsStore from '../stores/projectsStore.js'
+import ProjectItem from '../components/ProjectItem.vue'
+
+
+
+import { computed } from 'vue'
+
+const projectsStore = useProjectsStore();
+  
+const projects = computed(() => {
+  return projectsStore.stateProjects();
+});
+  
 </script>
 
 <template>
-  
+  <div>
+		<div class="flex flex-col items-center">
+			<h3 class="text-xl">#02. Projects</h3>
+			<h4>Some of the projects I\'ve built</h4>
+			<ul class="w-full">
+				<project-item class="mx-auto" v-for="project in projects"
+					:key="project.id"
+					:title="project.title"
+					:img_1="project.img_1"
+					:img_2="project.img_2"
+					:img_3="project.img_3"
+					:link="project.link"
+					:gitHub="project.gitHub"
+					:tech="project.tech"
+				></project-item>
+			</ul>
+    </div>
+	</div>
 </template>

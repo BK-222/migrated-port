22ba4c02d3d98d638b7282df1e0c6b31adcf6d70
BK-222 <58715574+BK-222@users.noreply.github.com>
2024-03-27 08:02:05 +0000

    Readjusted the github svg path for testing

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -6,7 +6,7 @@
   <div>
     <nav class="flex mb-2 lg:mb-0">
       <ul class="flex lg:justify-center items-center w-full mt-1 font-bold text-sm overflow-x-auto">
-        <img class="h-8 w-12 lg:h-10 lg:w-14 m-1 lg:mr-28 bg-cyan-500" src="src/assets/favicons/github.svg">
+        <img class="h-8 w-12 lg:h-10 lg:w-14 m-1 lg:mr-28 bg-cyan-500" src="/src/assets/favicons/github.svg">
         <li class="m-3 lg:m-6 hover:text-cyan-500 delay-100">
           <router-link to="/about"><span class="text-cyan-500 pr-1">#01.</span>About</router-link>
         </li>
